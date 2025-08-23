ee96a19005bdf5a5c8ca06c59b840c7f0c9b99b5
minseo Jo <liging12@naver.com>
2025-08-23 16:49:59 +0900

    초기 설정: 백엔드

---
+++ live-bmw/backend/src/main/java/com/live_bmw/LiveBmwApplication.java
@@ -0,0 +1,13 @@
+package com.live_bmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBmwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveBmwApplication.class, args);
+	}
+
+}
---
+++ live-bmw/backend/src/test/java/com/live_bmw/LiveBmwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live_bmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveBmwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
