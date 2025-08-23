557e1f576cc5526edcdc75140b5627497d8391df
minseo Jo <liging12@naver.com>
2025-08-23 16:51:34 +0900

    패키지명 변경
    기존: live_bmw
    변경: livebmw

--- live-bmw/backend/src/main/java/com/livebmw/LiveBmwApplication.java
+++ live-bmw/backend/src/main/java/com/livebmw/LiveBmwApplication.java
@@ -1,4 +1,4 @@
-package com.live_bmw;
+package com.livebmw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- live-bmw/backend/src/test/java/com/livebmw/LiveBmwApplicationTests.java
+++ live-bmw/backend/src/test/java/com/livebmw/LiveBmwApplicationTests.java
@@ -1,4 +1,4 @@
-package com.live_bmw;
+package com.livebmw;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
